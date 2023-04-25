2c0a5f95ceb036682902fc487e694d9c69dd3ef5
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-04-25 13:16:26 +0700

    Build project with Maven

---
+++ src/main/java/lehieunghia/spring/registrationandlogin/BaiTap3Application.java
@@ -0,0 +1,13 @@
+package lehieunghia.spring.registrationandlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiTap3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiTap3Application.class, args);
+    }
+
+}
---
+++ src/test/java/lehieunghia/spring/registrationandlogin/BaiTap3ApplicationTests.java
@@ -0,0 +1,13 @@
+package lehieunghia.spring.registrationandlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiTap3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
